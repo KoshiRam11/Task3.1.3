9f9726777a94c6a7d1b77faad380fa42ddbee7f4
Рамиль <ramasafiullin@yandex.ru>
2023-02-14 15:03:17 +0300

    UPD2
    UPD2

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -5,27 +5,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.entities.User;
-import ru.kata.spring.boot_security.demo.service.RoleService;
-import ru.kata.spring.boot_security.demo.service.UserService;
+import ru.kata.spring.boot_security.demo.service.RoleServiceImpl;
+import ru.kata.spring.boot_security.demo.service.UserServiceImpl;
 
 
 @Controller
 @RequestMapping("/admin")
 
 public class AdminController {
-    private final UserService userService;
-    private final RoleService roleService;
+    private final UserServiceImpl userServiceImpl;
+    private final RoleServiceImpl roleServiceImpl;
 
-    public AdminController(UserService userService, RoleService roleService) {
-        this.userService = userService;
-        this.roleService = roleService;
+    public AdminController(UserServiceImpl userServiceImpl, RoleServiceImpl roleServiceImpl) {
+        this.userServiceImpl = userServiceImpl;
+        this.roleServiceImpl = roleServiceImpl;
     }
 
 
     @GetMapping()
 
     public String allUsers(Model model) {
-        model.addAttribute("users", userService.listUsers());
+        model.addAttribute("users", userServiceImpl.listUsers());
         return "admin/admin";
     }
 
@@ -33,28 +33,28 @@ public class AdminController {
     public String createUserForm(Model model) {
         User user = new User();
         model.addAttribute("user", user);
-        model.addAttribute("listRoles", roleService.listRoles());
+        model.addAttribute("listRoles", roleServiceImpl.listRoles());
         return "admin/create";
     }
     @PostMapping("/create")
     public String createUser(@ModelAttribute("user") User user) throws Exception {
-        userService.saveUser(user);
+        userServiceImpl.saveUser(user);
         return "redirect:/admin";
     }
-    @GetMapping("/delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public String deleteUser(@PathVariable("id") Long id) {
-        userService.deleteUser(id);
+        userServiceImpl.deleteUser(id);
         return "redirect:/admin";
     }
     @GetMapping("/update/{id}")
     public String updateUserForm(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("user", userService.getUser(id));
-        model.addAttribute("listRoles", roleService.listRoles());
+        model.addAttribute("user", userServiceImpl.getUser(id));
+        model.addAttribute("listRoles", roleServiceImpl.listRoles());
         return "admin/update";
     }
-    @PostMapping("/update")
+    @PutMapping("/update")
     public String updateUser(@ModelAttribute("user") User user) {
-        userService.updateUser(user);
+        userServiceImpl.updateUser(user);
         return "redirect:/admin";
     }
 }
--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -6,13 +6,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.kata.spring.boot_security.demo.entities.User;
-import ru.kata.spring.boot_security.demo.service.UserService;
+import ru.kata.spring.boot_security.demo.service.UserServiceImpl;
 
 @Controller
 public class UserController {
-    private final UserService userService;
-    public UserController(UserService userService) {
-        this.userService = userService;
+    private final UserServiceImpl userServiceImpl;
+    public UserController(UserServiceImpl userServiceImpl) {
+        this.userServiceImpl = userServiceImpl;
     }
 
     @GetMapping(value = "/user")
